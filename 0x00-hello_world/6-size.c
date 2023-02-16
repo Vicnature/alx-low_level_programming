da8e2245dfff10189955548e89d9baece78f850d
Vicnature <ombebavictor22@gmail.com>
2023-02-16 09:08:19 -0800

    six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-/**
- * main- C program that prints the size of various types on the computer it is compiled and run on
+/*prints size of various types on the computer it is conpiled and run on
  * Return: 0
  */
 int main(void)

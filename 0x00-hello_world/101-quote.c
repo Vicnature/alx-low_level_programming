3d8aaf26f096e3a3c961e9ab03631f0af10b2cb2
Vicnature <ombebavictor22@gmail.com>
2023-02-16 11:25:31 -0800

    eight

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
-#include <unistd.h>
+#include <stdio.h>
+
 /**
- * main - prints a string and standard error
- * Return: 1
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main(void)                                       {                                                    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+int main(void)
+{
+    return (0);
+}

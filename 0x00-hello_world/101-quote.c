0c603600388059f087fd02eaa0d5d4a0fd64375d
Vicnature <ombebavictor22@gmail.com>
2023-02-16 17:42:32 -0800

    eight

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,13 @@
- #include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+/**
+ * main - Entry point
+ * Return: 1.
+ */
 
-return (1);
-} 
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}

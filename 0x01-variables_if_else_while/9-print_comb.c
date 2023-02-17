e8378f9c6d6767dc26ffc8ec07d898b689afa819
Vicnature <ombebavictor22@gmail.com>
2023-02-17 07:49:44 -0800

    patience and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible single digit number combinations.
+ * main - Prints all possible single-digit number combinations.
  *
- * Return: 0
+ * Return: 0.
  */
 int main(void)
 {
